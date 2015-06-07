b11b5b63216d4d76f1fe0adb4304ab2ab3d47219
Marcin Kaciuba <aldor007@gmail.com>
2015-06-07 11:10:19 +0200

    fix mock

--- tests/mocks/IdentityTranslatorMock.php
+++ tests/mocks/IdentityTranslatorMock.php
@@ -7,15 +7,15 @@ class IdentityTranslator {
         echo "***************";
         return $text;
     }
-    public function transchoice($text, $count, $options {
+    public function transchoice($text, $count, $options = array()) {
         echo "***************";
         $text = explode('|]1,Inf[ ', $text);
 
         if ($count == 1) {
-            $text[0] = preg_replace(/\{1\}/, '', $text[0]);
-            return $text[0]
+            $text[0] = preg_replace('/\{1\}/', '', $text[0]);
+            return $text[0];
         }
-        for ($options as $k => $v) {
+        foreach ($options as $k => $v) {
             $text[1] = preg_replace($k, $v, $text[1]);
         }
 
